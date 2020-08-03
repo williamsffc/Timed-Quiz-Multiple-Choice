0b725eb89e582550a400982907c9e49b9b0d6437
W F <williamsffc@gmail.com>
2020-08-02 20:43:08 -0700

    Final Draft

--- script.js
+++ script.js
@@ -7,106 +7,99 @@ var bottomDiv = document.getElementById("#bottomDiv");
 
 var quiz = document.getElementById("#quizDiv");
 var question = document.getElementById("#questions");
-var choiceA = document.getElementById("#A");
-var choiceB = document.getElementById("#B");
-var choiceC = document.getElementById("#C");
-var choiceD = document.getElementById("#D");
+var choiceA = document.getElementById("#btn-0");
+var choiceB = document.getElementById("#btn-1");
+var choiceC = document.getElementById("#btn-2");
+var choiceD = document.getElementById("#btn-3");
 
 var startBttn = document.getElementById("#button1");
 
-
 var questionsQuiz = [
     {
-        question: "Which of the following is not a valid JavaScript variable name?",
-        choiceA: "2names",
-        choiceB: "_first_and_last_names",
-        choiceC: "FirstAndLast",
-        choiceD: "None of the above",
-        answer: "A",
+        questionTitle: "Which of the following is not a valid JavaScript variable name?",
+        questionChoices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+        correctAnswer: "A",
     },
     {
-        question: "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
-        choiceA: "&ltbody&gt",
-        choiceB: "&ltscript&gt",
-        choiceC: "&lttittle&gt",
-        choiceD: "&lthead&gt",
-        answer: "B",
+        questionTitle: "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
+        questionChoices: ["<body>", "<script>", "<tittle>", "<head>"],
+        correctAnswer: "B",
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choiceA: "&ltjs&gt",
-        choiceB: "&ltscripting&gt",
-        choiceC: "&ltscript&gt",
-        choiceD: "&ltjavascript&gt",
-        answer: "C",
+        questionTitle: "Inside which HTML element do we put the JavaScript?",
+        questionChoices: ["<js>", "<scripting>", "<script>", "<javascript>"],
+        correctAnswer: "C",
     },
     {
-        question: "The _______ method of an Array object adds and / or removes elements from an array.",
-        choiceA: "Splice",
-        choiceB: "Slice",
-        choiceC: "Reverse",
-        choiceD: "Shift",
-        answer: "A"
+        questionTitle: "The _______ method of an Array object adds and / or removes elements from an array.",
+        questionChoices: ["Splice", "Slice", "Reverse", "Shift"],
+        correctAnswer: "A",
     },
 ];
 
-button1.addEventListener("click", startTimer)
+button1.addEventListener("click", startTimer);
 
 // --- TIMER ----
 function startT() {
-    var counter = 10;
+    var counter = 20;
     setInterval(function () {
         counter--;
-        if (counter >= 0) {
+        if (counter > 0) {
             span = document.getElementById("count");
             span.innerHTML = counter;
         }
         if (counter === 0) {
-            // alert('sorry, out of time');
             clearInterval(counter);
         }
-    }, 1000);
-}
+        if (counter < 1) {
+            clearInterval(counter);
+            span.innerHTML = "--";
+            alert("PLEASE TRY AGAIN...");
+            alert = function () { };
+        }
+    }, 850);
+};
+
+var lastQuestion = questionsQuiz.length;
+var currentQ = 0;
 
 //--- QUESTIONS AND ANSWERS DISPLAY
-var lastQuestion = questionsQuiz.length + 1;
-var currentQuestion = 0;
+function displayQ() {
+    document.getElementById("questionTitle").textContent = questionsQuiz[currentQ].questionTitle;
+    document.getElementById("btn-0").textContent = questionsQuiz[currentQ].questionChoices[0];
+    document.getElementById("btn-1").textContent = questionsQuiz[currentQ].questionChoices[1];
+    document.getElementById("btn-2").textContent = questionsQuiz[currentQ].questionChoices[2];
+    document.getElementById("btn-3").textContent = questionsQuiz[currentQ].questionChoices[3];
 
-function renderQuestion() {
-    var q = questionsQuiz[currentQuestion];
-    questions.innerHTML = "<p>" + q.question + "</p";
-    A.innerHTML = q.choiceA;
-    B.innerHTML = q.choiceB;
-    C.innerHTML = q.choiceC;
-    D.innerHTML = q.choiceD;
+    for (var i = 0; i < questionsQuiz.length; i++) {
+        document.getElementById("btn-" + i).textContent = questionsQuiz[currentQ].questionChoices[i];
+    }
 };
+
 
 function startTimer() {
     button1.style.display = "none";
-    renderQuestion();
     quizDiv.style.display = "block";
+    displayQ();
     startT();
 };
 
-// CHECK FOR CORRECT ANSWERS
-
+// ANSWER CHECK
 function answerMatch(choice) {
-    if (choice == questionsQuiz[currentQuestion].answer) {
-        currentQuestion++;
-    };
+    if (choice == questionsQuiz[currentQ].correctAnswer) {
+        alert("You guessed it right! :)")
+        currentQ++;
+    }
+    if (currentQ < lastQuestion) {
+        // currentQ++;
+        displayQ();
 
-    if (currentQuestion < lastQuestion) {
-        renderQuestion();
-        clearInterval(startT);
-      
-        console.log(choice)
-    };
-
-    // if (currentQuestion === 0) {
-    // quizDiv.style.display = "none";
-    // button1.style.display = "block";
-    // }
-
+    } else {
+        quizDiv.style.display = "none";
+        button1.style.display = "block";
+        clearInterval(counter);
+        alert("The quiz has ended... Good luck!!!");
+    }
 }
 
 
